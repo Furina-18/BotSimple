ac6b873fa4a4ea104ce64f52ff47f25c5024eec1
Furina-18 <accclone182th@gmail.com>
2025-04-18 07:04:20 +0700

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -18,8 +18,13 @@ def home():
 
 def start_flask_server():
     # Start Flask app with HTTPS
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=443, ssl_context=('cert.pem', 'key.pem'))
 
+def keep_alive():
+    thread = Thread(target=run)
+    thread.start()
+    
 def start_discord_bot():
     # Run the Discord bot in a separate thread
     subprocess.run([sys.executable, 'bot.py'])
