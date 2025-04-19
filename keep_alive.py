4279ce95f527dccc512e242bc6fb8812d7235703
Furina-18 <accclone182th@gmail.com>
2025-04-19 12:25:27 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,36 +1,15 @@
 from flask import Flask
-from flask_sslify import SSLify
-import threading
-import subprocess
-import sys
-from dotenv import load_dotenv
 from threading import Thread
-import os
 
-# Load environment variables
-load_dotenv()
-
-# Flask app setup
-app = Flask(__name__)
-sslify = SSLify(app)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running",200
+    return "I'm alive!", 200
 
-def start_flask_server():
-    # Start Flask app with HTTPS
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
-def start_web():
-    Thread(target=start_flask_server, daemon=True).start()
-    
-def start_discord_bot():
-    # Run the Discord bot in a separate thread
-    subprocess.run([sys.executable, 'bot.py'])
-
-if __name__ == "__main__":
-    # Run both Flask server and bot in separate threads
-    threading.Thread(target=start_flask_server, daemon=True).start()
-    threading.Thread(target=start_discord_bot, daemon=True).start()
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
